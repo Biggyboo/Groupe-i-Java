2f7c46a51fbf64f013e66b9eaeb0dfbda24b3ed8
Ice-cu8e <icecu8e44@gmail.com>
2019-01-17 08:22:08 +0100

    Add files via upload

---
+++ MSPR/src/main/java/org/ss/MSPR/MsprApplication.java
@@ -0,0 +1,14 @@
+package org.ss.MSPR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprApplication.class, args);
+	}
+
+}
+
