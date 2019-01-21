b7a88f469419a357341f2f282338a55cbcf57764
Ice-cu8e <icecu8e44@gmail.com>
2019-01-21 16:05:23 +0100

    Add files via upload

---
+++ API/src/main/java/com/mspr/API/POJO/Role.java
@@ -0,0 +1,39 @@
+package com.mspr.API.POJO;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    private long roleId;
+    private String libelle;
+    //private Collection<Personne> personnes;
+
+    public Role() {
+    }
+
+    public Role(String libelle) {
+        this.libelle = libelle;
+    }
+
+    @Id
+    @GeneratedValue
+    public long getRoleId() {
+    return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+    this.roleId = roleId;
+    }
+
+
+    public String getLibelle() {
+    return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+    this.libelle = libelle;
+    }
+}
---
+++ API/src/main/java/com/mspr/API/POJO/personneRepo.java
@@ -0,0 +1,7 @@
+package com.mspr.API.POJO;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface personneRepo extends CrudRepository<Personne, Long> {
+
+}
