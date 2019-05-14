8fd492c09293ed233a659206915da5fe8f9fc16f
Yann <cloarecyann@gmail.com>
2019-05-14 09:12:28 +0200

    Authentication

--- API/src/main/java/com/mspr/API/Authentication.java
+++ API/src/main/java/com/mspr/API/Authentication.java
@@ -3,20 +3,59 @@ package com.mspr.API;
 import com.mspr.API.POJO.Personne;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/authentication")
 @SessionAttributes("authentication")
 public class Authentication {
+    private int status = 0;
+    private boolean switchSatus = false;
     public static Personne personne;
 
-    @RequestMapping(method= RequestMethod.GET, produces = {"application/json"}, value = "/logout")
+    private void changeStatus(int value) {
+        if (!switchSatus) {
+            status = value;
+            switchSatus = true;
+        }
+    }
+
+    @RequestMapping(method= RequestMethod.POST, produces = {"application/json"})
     @ResponseStatus(HttpStatus.OK)
-    public static void logout() {
-        personne = null;
+    public @ResponseBody
+    String run(@RequestBody String identifiant) {
+        String response = "";
+        switchSatus = false;
+        try {
+            checkIdentifiant(identifiant);
+        } catch (Exception e) {
+            e.printStackTrace();
+            changeStatus(1);
+        }
+        switch (status) {
+            case 0: response = "Problème majeur";
+                break;
+            case 1:  response = "Connexion OK";
+                break;
+            case 2:  response = "Identifiant incorrect";
+                break;
+            case 3:  response = "Erreur connexion";
+                    break;
+        }
+        return response;
+    }
+
+    private void checkIdentifiant(String identifiant) {
+        Personne personne = new Personne()  ;
+        try{
+            if(!(personne.findPersonneByIdentifiant(identifiant)==null)){
+                changeStatus(1);
+            }else{
+                changeStatus(2);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            changeStatus(3);
+        }
     }
 }
